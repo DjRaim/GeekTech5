76c0cea99a38a68fa6d5ee082ad6b75d237de67f
DjRaim <raimdj@icloud.com>
2023-03-11 18:29:36 +0600

    hw6

---
+++ hw6/src/pages/countPage/CountPage.js
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { countNumber } from "../store/countSlice"
+
+
+function CountPage() {
+
+    const dispatch = useDispatch()
+    const [ valueOne, setValueOne ] = useState('')
+    const [ valueTwo, setValueTwo ] = useState('')
+    const { result } = useSelector( state => state.countReducer)
+
+    const chageSigns = (e) => {
+        // значение кнопок (+-*/)
+        const event = e.target.innerText
+
+        dispatch(countNumber({event, valueOne, valueTwo}))
+
+        //проверка пустой строки
+        if ( valueOne !== '' && valueTwo !== '' ) {
+            setValueOne('')
+            setValueTwo('')
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <input
+                    type='number'
+                    placeholder="number"
+                    value={valueOne}
+                    onChange={
+                        (e) => setValueOne(e.target.value)
+                    }
+                />
+
+                <input type='number' placeholder="number" value={valueTwo} onChange={(e) => setValueTwo(e.target.value)}/>
+
+            </div>
+
+            <button onClick={chageSigns}> + </button>
+            <button onClick={chageSigns}> - </button>
+            <button onClick={chageSigns}> * </button>
+            <button onClick={chageSigns}> / </button>
+
+            <div>
+                {result}
+            </div>
+        </>
+    )
+}
+
+export default CountPage
---
+++ hw6/src/store/countSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const countSlice = createSlice({
+    name: "countSlice",
+    initialState: {
+        result: ''
+    },
+    reducers: {
+        countNumber: ( state, action ) => {
+
+            let value1 = action.payload.valueOne
+            let value2 = action.payload.valueTwo
+
+            //проверка на пустые поля
+            if ( value1 === '' || value2 === '' ) {
+                state.result = "Заполните все поля"
+                return state
+            }
+
+            switch (action.payload.event) {
+                case "+":
+                    state.result = +value1 + +value2
+                    return state
+
+                case "-":
+                    state.result = +value1 - +value2
+                    return state
+
+                case "*":
+                    state.result = +value1 * +value2
+                    return state
+
+                case "/":
+                    if (+value2 === 0 ) {
+                        state.result = "На ноль делить нельзя"
+                        return state
+                    }
+                    state.result = +value1 / +value2
+                    return state
+
+                default: return state
+            }
+
+        },
+    }
+});
+
+export const { countNumber } = countSlice.actions;
+export default countSlice.reducer;
